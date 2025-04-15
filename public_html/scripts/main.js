f31a5559fc6adc6573b1f556af3bf70521b753a1
Jan Tarczoń <jan.tarczon@student.uj.edu.pl>
2025-04-15 10:29:39 +0200

    Dodano pasek nawigacji oraz podstawowe komponenty obsługi webGL

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -2,35 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
-async function parseJSON(){
-    const url = "https://api.sampleapis.com/coffee/hot";
-    
-    try{
-        const response = await fetch(url);
-        if(!response.ok) {
-            throw new Error('Response status: ${response.status}');
-        }
-        
-        
-        const coffee = await response.json();
-        console.log(coffee);
-        
-        let newArticle;
-        const part = document.getElementsByClassName("content")[0];
-        
-        for(const coffeeItem of coffee){
-            newArticle = `<h2>${coffeeItem.title}</h2>`;
-            newArticle += `<p>${coffeeItem.description}<\p>`;
-            newArticle += `<hr class="articleBreak">`;
-            
-            part.insertAdjacentHTML("beforeend",newArticle);
-        }
-        
-    }catch(error) {
-        console.error(error.message);
-    }
-    
-    
-}
 
-parseJSON();
+function dropDownMobileMenu(){
+    const menu = document.getElementsByClassName("topnav")[0];
+    if(menu.className === "topnav"){
+        menu.className += ' mobileVisible';
+        document.getElementsByClassName("icon")[0].innerHTML = "Zwiń";
+    } else {
+        menu.className = 'topnav';
+        document.getElementsByClassName("icon")[0].innerHTML = "Rozwiń";
+    }
+}
