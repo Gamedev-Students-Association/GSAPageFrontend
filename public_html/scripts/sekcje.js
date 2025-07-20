dc9735ca93e2e123f45a2ed52cefa36033ebf8dc
Jan Tarczoń <jan.tarczon@student.uj.edu.pl>
2025-07-20 22:16:01 +0200

    Dodano wczytywanie bufora i shaderow tla

--- public_html/scripts/sekcje.js
+++ public_html/scripts/sekcje.js
@@ -4,5 +4,18 @@
  */
 import {loadBackground} from "./webgl-basic.js"
 
-loadBackground();
+loadBackground('\
+\n\attribute vec4 vPosition;\n\
+\n\
+void main() {\n\
+            gl_Position = vPosition;\n\
+        }','#ifdef GL_ES\n\
+precision mediump float;\n\
+#endif\n\
+\n\
+uniform float u_time;\n\
+\n\
+void main() {\n\
+	gl_FragColor = vec4(0.991,0.324,1.000,1.000);\n\
+}');
 
--- public_html/scripts/webgl-basic.js
+++ public_html/scripts/webgl-basic.js
@@ -18,4 +18,66 @@ export async function loadBackground(VertexShader, FragmentShader){
     
     backgroundOpenGLContext.clearColor(1.0, 1.0, 1.0, 1.0);
     backgroundOpenGLContext.clear(backgroundOpenGLContext.COLOR_BUFFER_BIT);
+    
+    const backgroundBuffer = backgroundOpenGLContext.createBuffer();
+    backgroundOpenGLContext.bindBuffer(backgroundOpenGLContext.ARRAY_BUFFER, backgroundBuffer);
+    
+    const screenVertices = new Float32Array(8);
+    screenVertices[0] = -1;
+    screenVertices[1] = 1;
+    screenVertices[2] = 1;
+    screenVertices[3] = 1;
+    screenVertices[4] = 1;
+    screenVertices[5] = -1;
+    screenVertices[6] = -1;
+    screenVertices[7] = -1;
+    
+    backgroundOpenGLContext.bufferData(backgroundOpenGLContext.ARRAY_BUFFER, screenVertices.length * screenVertices.BYTES_PER_ELEMENT, backgroundOpenGLContext.STATIC_DRAW);
+    backgroundOpenGLContext.bufferData(backgroundOpenGLContext.ARRAY_BUFFER, screenVertices, backgroundOpenGLContext.STATIC_DRAW);
+    
+    const backgroundVertexShader = backgroundOpenGLContext.createShader(backgroundOpenGLContext.VERTEX_SHADER);
+    backgroundOpenGLContext.shaderSource(backgroundVertexShader, VertexShader);
+    backgroundOpenGLContext.compileShader(backgroundVertexShader);
+    
+    const backgroundFragmentShader = backgroundOpenGLContext.createShader(backgroundOpenGLContext.FRAGMENT_SHADER);
+    backgroundOpenGLContext.shaderSource(backgroundFragmentShader, FragmentShader);
+    backgroundOpenGLContext.compileShader(backgroundFragmentShader);
+    
+    const backgroundProgram = backgroundOpenGLContext.createProgram();
+    
+    backgroundOpenGLContext.attachShader(backgroundProgram, backgroundVertexShader);
+    backgroundOpenGLContext.attachShader(backgroundProgram, backgroundFragmentShader);
+    
+    backgroundOpenGLContext.linkProgram(backgroundProgram);
+    backgroundOpenGLContext.useProgram(backgroundProgram);
+    
+    let OpenGLExposedVariables = new Object;
+    OpenGLExposedVariables['attributes'] = {};
+    OpenGLExposedVariables['uniforms'] = {};
+    
+    let lineWords;
+    const vertexShaderLines = VertexShader.split('\n');
+    for(let line = 0; line < vertexShaderLines.length; ++line){
+        lineWords = vertexShaderLines[line].split(' ');
+        if(lineWords[0] === 'attribute'){
+            OpenGLExposedVariables['attributes'][lineWords[2].slice(0,-1)] =  backgroundOpenGLContext.getAttribLocation(backgroundProgram, lineWords[2].slice(0,-1));
+        }
+        if(lineWords[0] === 'uniform'){
+            OpenGLExposedVariables['uniforms'][lineWords[2].slice(0,-1)] =  backgroundOpenGLContext.getAttribLocation(backgroundProgram, lineWords[2].slice(0,-1));
+        }
+    }
+    
+
+    const fragmentShaderLines = FragmentShader.split('\n');
+    for(let line = 0; line < fragmentShaderLines.length; ++line){
+        lineWords = fragmentShaderLines[line].split(' ');
+        if(lineWords[0] === 'attribute'){
+            OpenGLExposedVariables['attributes'][lineWords[2].slice(0,-1)] =  backgroundOpenGLContext.getAttribLocation(backgroundProgram, lineWords[2].slice(0,-1));
+        }
+        if(lineWords[0] === 'uniform'){
+            OpenGLExposedVariables['uniforms'][lineWords[2].slice(0,-1)] =  backgroundOpenGLContext.getAttribLocation(backgroundProgram, lineWords[2].slice(0,-1));
+        }
+    }
+    
+    console.log(OpenGLExposedVariables);
 }
